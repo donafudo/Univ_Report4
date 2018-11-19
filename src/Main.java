f2b08315faf9d6deaf63829ffd4aa1e2125d1831
tawatamasato <donafudo@gmail.com>
2018-11-19 15:18:55 +0900

    Added exception message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch (Exception e){
+            System.out.println(e);
+            System.out.println(e.getMessage());
+        }
+
     }
 }
